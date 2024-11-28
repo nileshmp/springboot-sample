ea0c86cbb603c043bf9cb1746cf2d8435cdc5608
Nilesh M P <nileshmp@gmail.com>
2024-11-28 18:59:06 +0530

    enabling debug logs

--- src/main/java/com/nilesh/sample/controller/HelloController.java
+++ src/main/java/com/nilesh/sample/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hotels")
+@RequestMapping("/sample/app")
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
